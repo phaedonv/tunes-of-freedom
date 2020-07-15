7b436adbdf71e7435cf894cdc708abd56519975d
Phaedon Valasiadis <faival@protonmail.com>
2020-07-15 19:29:26 +0300

    added devise and user model..needs calibration..

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,7 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+# config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+     # In production, :host should be set to the actual host of your application.
