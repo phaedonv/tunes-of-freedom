b9066ea0ba91aa7c0b53bd1181b9d8247963f71f
Phaedon Valasiadis <faival@protonmail.com>
2020-07-31 01:57:48 +0300

    test

--- db/seeds.rb
+++ db/seeds.rb
@@ -22,7 +22,7 @@ puts "..........................."
 puts "now creating admin!"
 p admin = User.create!(
   email: "admin@admin.com",
-  password: 'EnterYourPassword',
+  password: "useradmin",
   name: "nemo",
   admin: true
   )
