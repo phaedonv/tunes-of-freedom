845f477f7fa45c94e24e5edec9ce10eecae84fe1
Phaedon Valasiadis <faival@protonmail.com>
2021-02-12 11:47:56 +0100

    redirection after login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,11 @@ class ApplicationController < ActionController::Base
 
   include Pundit
 
+  def after_sign_in_path_for(resource)
+    user_path(current_user) #redirection to current user page after login with devise
+  end
+
+
   private
 
   def configure_permitted_parameters
